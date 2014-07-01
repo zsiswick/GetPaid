f79a364050fdd5a22371db5955fc015e44efe5b0
zsiswick <zsiswick@BOS-ZSiswick.local>
2014-07-01 09:48:42 -0400

    Header changes for Foundation

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Get Paid!</title>
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/foundation.css" />
-	<!--<link rel="stylesheet" href="<?php $autoload['helper'] = array('url','utility'); ?>assets/css/company.css" />-->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
-	<script src="<?php echo base_url(); ?>assets/js/vendor/modernizr.js"></script>
-</head>
-<body>
-<nav class="top-bar">
-	<ul class="title-area">
-    <li class="name">
-      <h1><?php echo anchor('/home', 'Home'); ?></h1>
-    </li>
-  </ul>
-	<section class="top-bar-section">
-		<ul class="right">
-			<li><?php echo anchor('invoices', 'Invoices'); ?></li>
-			<li><?php echo anchor('clients', 'Clients'); ?></li>
-			<li><?php echo anchor('settings', 'Settings'); ?></li>
-		</ul>
-	</section>
-</nav>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<title>Get Paid!</title>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/normalize.css" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/foundation.css" />
+		<!--<link rel="stylesheet" href="<?php $autoload['helper'] = array('url','utility'); ?>assets/css/company.css" />-->
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
+		<script src="<?php echo base_url(); ?>assets/js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+	<nav class="top-bar" data-topbar>
+		<ul class="title-area">
+	    <li class="name">
+	      <h1><?php echo anchor('/home', 'Home'); ?></h1>
+	    </li>
+	    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+	  </ul>
+		<section class="top-bar-section">
+			<ul class="right show-for-large-up">
+				<li><?php echo anchor('invoices', 'Invoices'); ?></li>
+				<li><?php echo anchor('clients', 'Clients'); ?></li>
+				<li><?php echo anchor('settings', 'Settings'); ?></li>
+			</ul>
+			<ul class="right hide-for-large-up">
+				<li><?php echo anchor('invoices', 'Invoices'); ?></li>
+				<li><?php echo anchor('clients', 'Clients'); ?></li>
+				<li><?php echo anchor('settings', 'Settings'); ?></li>
+			</ul>
+		</section>
+	</nav>
