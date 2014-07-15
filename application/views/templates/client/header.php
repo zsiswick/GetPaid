f5a5eaad6bbe3c8b9b89b452e4549ad125369f05
zsiswick <zsiswick@bos-zsiswick.home>
2014-07-15 07:20:32 -0400

    Client Header/Footer

---
+++ application/views/templates/client/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<title>GetPaid!</title>
+		<link href='http://fonts.googleapis.com/css?family=Fenix' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/normalize.css" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/foundation.css" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/foundation-icons.css" />
+		<!--<link rel="stylesheet" href="<?php $autoload['helper'] = array('url','utility'); ?>assets/css/company.css" />-->
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" />
+		<script src="<?php echo base_url(); ?>assets/js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+	<section role="main" class="outer-wrap">
