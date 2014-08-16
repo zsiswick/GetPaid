ea7438087074ef4bdd90a5561e3d564f214def7f
zsiswick <zsiswick@bos-zsiswick.home>
2014-08-16 17:07:29 -0400

    CSS Tweaks
    Made the add client ajax form prettier

--- application/views/pages/clients/create-ajax.php
+++ application/views/pages/clients/create-ajax.php
@@ -1,32 +1,71 @@
-<div class=" clearfix">
-	<div class="">
-		<?php echo validation_errors(); ?>
-		
+<div class="clearfix light-bg">
+	<?php echo validation_errors(); ?>
 		
 		<?php 
 			$attributes = array('id' => 'addClient');			
 			echo form_open('clients/create', $attributes) 
 		?>
-		
-			<input type="text" name="company" placeholder="Company" />
-			<input type="text" name="contact" placeholder="Contact Name" />
-			<input type="text" name="email" placeholder="Email Address" />
-			
-			<div id="moreFields">
-				<input type="text" name="phone" placeholder="Phone Number" />
-				<input type="text" name="address_1" placeholder="Address 1" />
-				<input type="text" name="address_2" placeholder="Address 2" />
-				<input type="text" name="city" placeholder="City"/>
-				<input type="text" name="state" placeholder="State"/>
-				<input type="text" name="zip" placeholder="zip"/>
-				<input type="text" name="country" placeholder="Country"/>
-				<input type="text" name="tax_id" placeholder="Tax ID" />
-				<input type="text" name="default_inv_prefix" placeholder="Invoice Prefix"/>
+		<h2 class="text-center">Add a Client</h2>
+		<div class="row">
+			<div class="medium-6 columns">
+				<input type="text" name="company" placeholder="Company" />
 			</div>
-			<a id="showMoreFields" href="#" class="button round secondary">Add more info</a>
-			<input type="submit" name="submit" value="Add Client" class="button round" />
-		</form>
-	</div>
+			<div class="medium-6 columns">
+				<input type="text" name="contact" placeholder="Contact Name" />
+			</div>
+			<div class="medium-12 columns end">
+				<input type="text" name="email" placeholder="Email Address" />
+			</div>
+		</div>
+		
+		<div id="moreFields">
+			<div class="row">
+				<div class="medium-12 columns">
+					<input type="text" name="phone" placeholder="Phone Number" />
+				</div>
+				<div class="medium-6 columns">
+					<input type="text" name="address_1" placeholder="Address 1" />
+				</div>
+				<div class="medium-6 columns">
+					<input type="text" name="address_2" placeholder="Address 2" />
+				</div>
+				<div class="medium-6 columns">
+					<input type="text" name="city" placeholder="City"/>
+				</div>
+				<div class="medium-3 columns">
+					<input type="text" name="state" placeholder="State"/>
+				</div>
+				<div class="medium-3 columns">
+					<input type="text" name="zip" placeholder="zip"/>
+				</div>
+				<div class="medium-12 columns">
+					<input type="text" name="country" placeholder="Country"/>
+				</div>
+				<div class="medium-6 columns">
+					<input type="text" name="tax_id" placeholder="Tax ID" />
+				</div>
+				<div class="medium-6 columns">
+					<input type="text" name="default_inv_prefix" placeholder="Invoice Prefix"/>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="small-12 columns">
+				<a id="showMoreFields" href="#">Add more info</a>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-12 columns text-center">
+				<hr />
+				<input type="submit" name="submit" value="Add Client" class="button round" />
+			</div>
+		</div>
+		
+		
+		
+		
+	</form>
 </div>		
 <a class="close-reveal-modal">&#215;</a>
 <script type="text/javascript">
